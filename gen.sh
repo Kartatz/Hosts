c0543596f93aff2bb9c745efd3b33b53ab438b82
Kartatz <105828205+Kartatz@users.noreply.github.com>
2024-09-15 18:08:40 -0300

    Initial commit

---
+++ gen.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+curl \
+	--url 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/hosts/ultimate.txt' \
+	--url 'https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social-only/hosts' \
+	--url 'https://badmojr.github.io/1Hosts/mini/hosts.txt' \
+	--url 'https://raw.githubusercontent.com/badmojr/1Hosts/master/Pro/hosts.txt' \
+	--url 'https://adaway.org/hosts.txt' \
+	--url 'https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts' \
+	--url 'https://raw.githubusercontent.com/AdroitAdorKhan/EnergizedProtection/master/core/hosts' \
+	--url 'https://raw.githubusercontent.com/Yhonay/antipopads/master/hosts' \
+	--url 'https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt' \
+	--url 'https://raw.githubusercontent.com/bjornstar/hosts/master/hosts' \
+	--url 'https://0xacab.org/my_privacy_dns/clefspeare13_pornhosts/-/raw/master/download_here/0.0.0.0/hosts' \
+	--url 'https://raw.githubusercontent.com/MetaMask/eth-phishing-detect/master/src/hosts.txt' \
+	--url 'https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts' \
+	--url 'https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Dead/hosts' \
+	--url 'https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts' \
+	--url 'https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts' \
+	--url 'https://raw.githubusercontent.com/HexxiumCreations/threat-list/gh-pages/hosts.txt' \
+	--url 'https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/hosts-VN' \
+	--url 'https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt' \
+	--url 'https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt' \
+	--url 'https://hblock.molinero.dev/hosts' \
+	--url 'https://cdn.jsdelivr.net/gh/neoFelhz/neohosts@gh-pages/basic/hosts.txt' \
+	--url 'https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt' \
+	--url 'https://pgl.yoyo.org/adservers/serverlist.php?showintro=0;hostformat=hosts' \
+	--url 'https://raw.githubusercontent.com/mhxion/pornaway/master/hosts/porn_sites.txt' \
+	--url 'https://raw.githubusercontent.com/Sinfonietta/hostfiles/master/pornography-hosts' \
+	--url 'https://someonewhocares.org/hosts/hosts' \
+	--url 'http://sbc.io/hosts/hosts' \
+	--url 'https://raw.githubusercontent.com/StevenBlack/hosts/master/data/StevenBlack/hosts' \
+	--url 'https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt' \
+	--url 'https://winhelp2002.mvps.org/hosts.txt' \
+	--url 'https://raw.githubusercontent.com/yous/YousList/master/hosts.txt' \
+	--fail \
+	--silent \
+	--doh-url 'https://1.0.0.1/dns-query' \
+	--ipv4 \
+	--connect-timeout '8' \
+	--header 'User-Agent:' \
+	--header 'Accept:' \
+	| sed -r '/(<|>|\t|#|\}|\)|;)/d; s/\\//g; s/\r//g; s/^127\.0\.0\.1/0.0.0.0/g; s/^0\.0\.0\.0/::/g' \
+	| awk 'NF && !seen[$0]++' \
+	> 'hosts'
+
+python -B -u hosts.py
+
+mv './hosts2' './hosts'
---
+++ hosts.py
@@ -0,0 +1,131 @@
+import asyncio
+import functools
+import socket
+
+import aiofiles
+
+def gethostname(line):
+	
+	(
+		address,
+		hostname
+	) = (
+		line
+			.split(sep = " ", maxsplit = 1)
+	)
+	
+	host = hostname.strip()
+	
+	return host
+
+async def agetaddrinfo(host):
+	
+	res = None
+	
+	loop = asyncio.get_running_loop()
+	
+	func = functools.partial(
+		socket.getaddrinfo,
+		host = host,
+		port = 80
+	)
+	
+	try:
+		res = await loop.run_in_executor(None, func)
+	except socket.gaierror:
+		pass
+	
+	return (host, res)
+
+async def main():
+	
+	tasks = []
+	mapping = {}
+	
+	finished = False
+	
+	async with (
+		aiofiles.open(file = "./hosts", mode = "r") as file,
+		aiofiles.open(file = "./hosts2", mode = "w") as file2
+	):
+		iterable = aiter(file)
+		
+		while True:
+			line = await anext(iterable)
+			host = gethostname(line = line)
+			
+			task = asyncio.create_task(
+				coro = agetaddrinfo(host = host)
+			)
+			tasks.append(task)
+			
+			if len(tasks) == 1024:
+				break
+		
+		task_index = -1
+		total_tasks = (len(tasks) - 1)
+		
+		while True:
+			task_index += 1
+			
+			if task_index > total_tasks:
+				task_index -= task_index
+				await asyncio.sleep(0.1)
+			
+			task = tasks[task_index]
+			
+			if task is None:
+				if all(task is None for task in tasks):
+					break
+				
+				continue
+			
+			done = task.done()
+			
+			if not done:
+				continue
+			
+			result = task.result()
+			
+			(host, addresses) = result
+			
+			if addresses is not None:
+				text = ":: %s\n" % (host)
+				await file2.write(text)
+				
+				occurrences = 0
+				
+				for address in addresses:
+					ip = address[-1][0]
+					occurrences = mapping.get(ip)
+					
+					if occurrences is None:
+						occurrences = 0
+					
+					occurrences += 1
+					
+					mapping.update(
+						{
+							ip: occurrences
+						}
+					)
+			
+			line = None
+			
+			try:
+				line = await anext(iterable)
+			except StopAsyncIteration:
+				finished = True
+			
+			tasks[task_index] = None if finished else asyncio.create_task(
+				coro = agetaddrinfo(
+					host = gethostname(line = line)
+				)
+			)
+	
+	text = json.dumps(obj = mapping)
+	
+	async with aiofiles.open(file = "./mapping.json", mode = "w") as file:
+		await file.write(text)
+
+asyncio.run(main())
